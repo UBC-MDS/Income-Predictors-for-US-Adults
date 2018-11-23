668f5f8664f98c7f1b663380080198b203d44117
unknown <dglin1@gmail.com>
2018-11-22 20:28:46 -0800

    Update data cleaning script

--- src/load_data.py
+++ src/load_data.py
@@ -1,5 +1,28 @@
-import pandas as pd
+# load_data.py
 
-census_data = pd.read_csv(r'./data/census_data.csv')
-census_snippet = census_data.head(10)
-print(census_snippet)
+import pandas as pd
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input_file')
+    parser.add_argument('output_file')
+    args = parser.parse_args()
+    
+    input_file = args.input_file
+    output_file = args.output_file
+    
+    COL_NAMES =['age', 'workclass', 'fnlwgt', 'education', 'education_num',
+                'marital_status', 'occupation', 'relationship', 'race',
+                'sex', 'capital_gain', 'capital_loss', 'hours_per_week',
+                'native_country', 'target']
+    census_data = pd.read_csv(input_file, skiprows=1, header=None,
+                              names=COL_NAMES)
+
+    # remove rows with missing info
+    census_data_filtered = census_data.query('workclass != " ?" and occupation != " ?" and native_country != " ?"')                          
+    
+    census_data_filtered.to_csv(output_file)
+
+if __name__ == '__main__':
+    main()
